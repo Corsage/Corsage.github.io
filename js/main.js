4d773fcc858c40ebf314f745bfeb9f460d1f99f6
Jay C <j3chowdh@edu.uwaterloo.ca>
2017-11-06 16:21:43 -0500

    Added commands.

--- js/main.js
+++ js/main.js
@@ -9,16 +9,32 @@ class Legacy {
         this.detour = document.getElementById('init');
         this.virtualProtect = document.getElementById('virtualprotect');
 
-        /** Constants */
-        this.CORSAGE = 'corsage';
+        /** Public commands that will be listed. */
+        this.publicCommandsList = ['HELP', 'ABOUT', 'PORTFOLIO', 'CONTACT', 'BLOG'];
+    }
+
+    generateHelp() {
+        let self = this;
+
+        let help = document.createElement('code');
+        help.innerHTML = 'Avaliable public commands: "about", "portfolio", "contact", "blog".';
+        self.virtualProtect.appendChild(help);
+
+        window.setTimeout(function() {
+            self.virtualProtect.removeChild(help);
+        }, 5000);
     }
 
     /** Command handler -- You should read this. */
     handleCommand(query) {
         console.log(`Client executed command: ${query}`);
-        switch (query) {
+        switch (query.toUpperCase()) {
+            case 'HELP':
+                this.generateHelp();
+                break;
+
             case '133 221 333 123 111':
-                console.log("Soon.");
+                window.location.assign("http://www.kusowiabu.pw");
                 break;
         }
     }
@@ -31,8 +47,14 @@ class Legacy {
         let client = document.createElement('textarea');
         client.setAttribute('id', 'client');
         client.setAttribute('rows', '1');
+        client.setAttribute('tabindex', '1');
+
+
+        let caption = document.createElement('p');
+        caption.innerHTML = 'Type "help" for help.';
 
         self.virtualProtect.appendChild(client);
+        self.virtualProtect.appendChild(caption);
 
         client.addEventListener('keypress', function(e) {
             // 'Enter' key.
@@ -43,8 +65,6 @@ class Legacy {
                 this.value = "";
             }
         }, false);
-
-        client.focus();
     }
 
     hook() {
@@ -53,6 +73,7 @@ class Legacy {
         self.detour.addEventListener('animationend', function(e) {
             self.secret();
             self.virtualProtect.classList.remove('hide');
+            document.getElementById('client').focus();
         }, false);
     }
 }
